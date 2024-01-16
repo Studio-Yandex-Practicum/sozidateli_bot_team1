6b6c5c01b8d8f70bbba1b01c9946a2ee0ce22a27
Анна Острицкая <ostr.anna@bk.ru>
2024-01-16 16:54:18 +0300

    Models of admin

---
+++ sozi_adm/meetings/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+
+from django.db import models
+from users.models import Candidate
+
+
+class Meeting(models.Model):
+    name = models.CharField(
+        max_length=150,
+        verbose_name='Название встречи',
+        help_text='Название встречи',
+    )
+
+    date_created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата и время создание встречи',
+        help_text='Дата и время создания встречи',
+    )
+
+    date_meeting = models.DateTimeField(
+        blank=True,
+        # default='2023-11-16 12:28:27.068357',
+        verbose_name='Дата и время проведения встречи',
+        help_text='Дата и время проведения встречи',
+    )
+
+    date_modified_at = models.DateTimeField(
+        auto_now_add=False,
+        auto_now=True,
+        verbose_name='Изменеие в дате/времени встречи',
+        help_text='Изменеие в дате/времени встречи',
+        )
+
+    location = models.URLField(
+        blank=True,
+        max_length=200,
+        verbose_name='Ссылка на встречу',
+        help_text='Ссылка на встречу',
+    )
+
+    candidate = models.ForeignKey(
+        Candidate,
+        related_name='candidate_for_valanture',
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        verbose_name = 'Встреча'
+        verbose_name_plural = 'Встречи'
+
+    def __str__(self):
+        return self.name
---
+++ sozi_adm/users/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class Candidate(AbstractUser):
+    email = models.EmailField(
+        max_length=254,
+        unique=True,
+        verbose_name='Адрес электронной почты',
+        help_text='Электронная почта',
+    )
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        verbose_name='Юзернейм',
+        help_text='Уникальный юзернейм',
+    )
+    first_name = models.CharField(
+        max_length=150,
+        verbose_name='Имя',
+        help_text='Имя',
+    )
+    last_name = models.CharField(
+        max_length=150,
+        verbose_name='Фамилия',
+        help_text='Фамилия',
+    )
+    password = models.CharField(
+        max_length=150,
+        verbose_name='Пароль',
+        help_text='Пароль',
+    )
+    phone = models.CharField(
+        max_length=12,
+        blank=True,
+        verbose_name='Номер телефона',
+        help_text='Номер телефона',
+    )
+
+    text = models.BooleanField(
+        verbose_name='Хотите познакомиться с нами на открытой встрече?',
+        help_text='Хотите познакомиться с нами на открытой встрече?',
+        default=True,
+    )
+
+    class Meta:
+        verbose_name = 'Кандидат'
+        verbose_name_plural = 'Кандидаты'
+
+    def __str__(self):
+        return self.username
+
