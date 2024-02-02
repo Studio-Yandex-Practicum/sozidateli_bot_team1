22f5a281aefed49b78a7ccb8abf1bfad211a7faa
Alexey Yatskevich <yatskevichas@gmail.com>
2024-02-02 20:12:08 +0000

    fix isort

--- tg_bot/handlers.py
+++ tg_bot/handlers.py
@@ -1,6 +1,5 @@
 import asyncio
 import re
-from tg_bot.parser import parse_meeting_date_time
 
 import requests
 from aiogram import F, Router
@@ -11,6 +10,7 @@ from requests.exceptions import ConnectionError
 
 import constants
 import keyboards
+from tg_bot.parser import parse_meeting_date_time
 from tg_bot.states import Edite, MonitoringDate, RegisterUser
 from tg_bot.utils import (create_keyboard, get_base_url, get_date_of_meeting,
                           is_admin, set_date_of_meeting)
